6dbbae11e8b9890794d88736fda611c557399da9
razak <razakwasiu@gmail.com>
2024-01-03 10:29:46 +0000

    add

--- app.js
+++ app.js
@@ -1,53 +1,59 @@
-'use strict';
+"use strict";
 
-const { App } = require('@slack/bolt');
-require('dotenv').config();
-const fs = require('fs');
-let raw = fs.readFileSync('./faqsDB.json');
-let faqs = JSON.parse(raw);
-
-const faqModule = require('./src/modules/faqModule');
-const updateModule = require('./src/modules/updateModule');
-const chatModule = require('./src/modules/chatModule');
+const { App } = require("@slack/bolt");
+require("dotenv").config();
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode:true, 
+  socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
 });
 
-app.command('/live', async ({ command, ack, say }) => {
+app.command("/live", async ({ command, ack, say }) => {
   try {
-    console.log('command>>>>>', command);
+    console.log("command>>>>>", command);
     await ack();
-    say('I am alive!!!');
+    say("I am alive!!!");
   } catch (error) {
-    console.log('err');
+    console.log("err");
     console.error(error);
   }
 });
 
-app.message(/hey/, async ({ command, say }) => { 
+app.message("hey", async ({ command, say }) => {
   try {
-    say('I noticed your hey, hey back.');
+    say("I noticed your hey, hey back.");
   } catch (error) {
-    console.log('err');
+    console.log("err");
     console.error(error);
   }
 });
 
-//** BRINGING IN MODULES */
-faqModule(app, faqs);
-updateModule(app, faqs);
-chatModule(app);
+async function sendDirectMessage(userId, messageText) {
+  try {
+    // Open a conversation with the specified user
+    const conversation = await app.client.conversations.open({
+      token: process.env.SLACK_BOT_TOKEN,
+      users: userId,
+    });
+
+    // Post a message to the opened conversation
+    const result = await app.client.chat.postMessage({
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: conversation.channel.id,
+      text: messageText,
+    });
+
+    console.log(result);
+  } catch (error) {
+    console.error("Error sending message:", error);
+  }
+}
 
 (async () => {
   const port = 3002;
   await app.start(process.env.PORT || port);
   console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+  sendDirectMessage("U06C780DSH2", "Hello there! This is a DM. during google meeting");
 })();
-
-
-
-
